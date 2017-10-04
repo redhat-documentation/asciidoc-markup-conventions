c15396e1f602d7c4604079b81422309934495d42
Lukas Ruzicka <lruzicka@redhat.com>
2017-10-04 14:23:45 +0200

    Rebuild docs.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+git pull --rebase
 asciidoctor asciidoc-markup-sample-doc/master.adoc  
 mv -f asciidoc-markup-sample-doc/master.html docs/index.html 
+git add .
+git commit -m "Rebuild docs."
+git push
