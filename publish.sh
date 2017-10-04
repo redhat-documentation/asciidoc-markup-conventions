81c52a55107b940b8eb3a6381c3c1bf8704ea419
Lukáš Růžička <lruzicka@redhat.com>
2017-10-04 20:56:48 +0200

    Comment the script.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #This script is only used to build the document with AsciiDoctor for publishing on Github pages.
+#If you want to publish your own version of the document on your Github.io account, learn how to serve content from the /docs directory.
 git pull 
 asciidoctor asciidoc-markup-sample-doc/master.adoc  
 mv -f asciidoc-markup-sample-doc/master.html docs/index.html 
