5c21bf2ea3b6a24cd7b13152c6f6236ab2b0c797
Robert Krátký <rkratky@redhat.com>
2017-11-06 19:16:53 +0100

    Update publ. script.

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@
 #This script is only used to build the document with AsciiDoctor for publishing on Github pages.
 #If you want to publish your own version of the document on your Github.io account, learn how to serve content from the /docs directory.
 git pull 
-asciidoctor asciidoc-markup-sample-doc/master.adoc  
-mv -f asciidoc-markup-sample-doc/master.html docs/index.html 
+asciidoctor asciidoc-markup-sample-doc/asciidoc-markup-samples.adoc  
+mv -f asciidoc-markup-sample-doc/asciidoc-markup-samples.html docs/index.html 
 git add .
 git commit -m "Rebuild docs."
 git push
